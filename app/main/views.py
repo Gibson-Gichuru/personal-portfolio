f20b54b4cd46ad9095251ed5efe5ef89db962b0b
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-10-02 13:38:28 +0300

    project files restructure

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,10 @@ from . import main
 def home():
 
     return render_template('/main/index.html')
+
+
+
+@main.route('/project/details')
+def project_details():
+
+    return render_template('/main/project_details.html')
